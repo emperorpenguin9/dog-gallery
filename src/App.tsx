f32b489d554ad0419df21bd1540723b2278c90e4
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-07-01 08:37:55 -0400

    Dog image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+type BreedObj = {
+  breed: string;
+  dogImgUrl: string;
+};
 
 function App() {
+  // Get dog breed data
+  const [breeds, setBreeds] = useState<BreedObj[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+      const data = await response.json();
+      const breeds = Object.keys(data.message);
+
+      const breedPromises = breeds.map(async (breed) => {
+        const imgURL = `https://dog.ceo/api/breed/${breed}/images/random`;
+        const res = await fetch(imgURL);
+        const imgData = await res.json();
+
+        return { breed, dogImgUrl: imgData.message as string };
+      });
+
+      const breedImages = await Promise.all(breedPromises);
+
+      setBreeds(breedImages);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <div id="js_loading">Loading....</div>
+      ) : (
+        <div id="js_gallery">
+          {!!breeds &&
+            breeds.map(({ breed, dogImgUrl }, idx) => (
+              <div className="gallery-item" key={idx}>
+                <img src={dogImgUrl} alt={breed} className="js_img" />
+                <div className="js_name">{breed}</div>
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 }
